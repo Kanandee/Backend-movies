6bfa89b72752a4d63f55c44a74c0a24c5ef8e057
Kanandee <lauritapeba@hotmail.com>
2023-01-15 20:41:02 +0100

    Añadido middelwares por roles

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,12 @@ const router = Router();
 
 import UserController from "../controllers/UserController.js";
 import isSuperAdmin from "../middelwares/isSuperAdmin.js";
+import isAdmin from "../middelwares/isAdmin.js";
+import isUser from "../middelwares/isUser.js";
 import verifyToken from "../middelwares/verifyToken.js";
 
 /* GET users listing. */
-//router.get("/", verifyToken, isSuperAdmin, UserController.getAll);
-router.get("/", UserController.getAll);
-router.get("/:email", UserController.getInfo);
+router.get("/", verifyToken, isSuperAdmin, UserController.getAll);
+router.get("/:email", verifyToken, isUser, UserController.getInfo);
 
 export default router;
